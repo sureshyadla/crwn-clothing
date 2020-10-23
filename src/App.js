c5fc6d859046f092c022aae0c793fca35c4ac7cc
Suresh Yadla <suresh.yadla@unilogcorp.com>
2020-10-23 14:42:28 -0400

    Updating our app with a shop page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx';
 import './pages/homepage/homepage.styles.scss';
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
